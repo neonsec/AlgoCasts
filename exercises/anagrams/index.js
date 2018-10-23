45917f3eda7ac5a2cf57fe83bde01bac66c0591a
Fahid M <fahidroid@gmail.com>
2018-10-23 22:44:53 +0530

    sol1 for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,30 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    stringA = stringA.replace(/[^\w]/g,"")
+    stringB = stringB.replace(/[^\w]/g,"")
+    if(stringA.length != stringB.length)
+        return false
+    var charMap = {};
+    var charMap2 = {};
+    for(var ch of stringA){
+        if(charMap[ch])
+            charMap[ch] +=1
+        else
+            charMap[ch] = 1
+    }
+    for(var ch of stringB){
+        if(charMap2[ch])
+            charMap2[ch] +=1
+        else
+            charMap2[ch] = 1
+    }
+    for(var ch in charMap){
+        if(charMap[ch] != charMap2[ch])
+            return false
+    }
+    return true
+}
 
 module.exports = anagrams;
