19c4ab7cd9cd2de5a5923baa9311f9f6535c9722
Fahid M <fahidroid@gmail.com>
2018-10-23 22:52:17 +0530

    cleaner

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,26 +7,18 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
-
+function buildCharMap(str){
+    var charMap = {}
+    for(var ch of str.replace(/[^\w]/g,"").toLowerCase()){        
+            charMap[ch] = charMap[ch] + 1 || 1    
+    }
+    return charMap
+}
 function anagrams(stringA, stringB) {
-    stringA = stringA.replace(/[^\w]/g,"")
-    stringB = stringB.replace(/[^\w]/g,"")
-    if(stringA.length != stringB.length)
-        return false
-    var charMap = {};
-    var charMap2 = {};
-    for(var ch of stringA){
-        if(charMap[ch])
-            charMap[ch] +=1
-        else
-            charMap[ch] = 1
-    }
-    for(var ch of stringB){
-        if(charMap2[ch])
-            charMap2[ch] +=1
-        else
-            charMap2[ch] = 1
-    }
+    var charMap = buildCharMap(stringA)
+    var charMap2 = buildCharMap(stringB)
+    if(Object.keys(charMap).length != Object.keys(charMap2).length)
+        return false;
     for(var ch in charMap){
         if(charMap[ch] != charMap2[ch])
             return false
