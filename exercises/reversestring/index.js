ee9bdfb8b2a2bfe913f849165789fcc44732719d
Fahid M <fahidroid@gmail.com>
2018-10-23 10:10:10 +0530

    2nd sol for reverse int

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,9 +5,17 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
-function reverse(str) {
-    return str.split("").reverse().join("");
+//sol 1
+// function reverse(str) {
+//     return str.split("").reverse().join("");
+// }
+ //sol 2
+ function reverse(str) {
+     let reverseStr = ""
+    for(var i= str.length-1; i>=0; i--)
+        reverseStr += str[i];       
+    return reverseStr;
 }
+
 
 module.exports = reverse;
