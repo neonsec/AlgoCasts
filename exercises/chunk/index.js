f98ad6a829395cd2e86067e951085555fed86518
Fahid M <fahidroid@gmail.com>
2018-10-23 21:17:08 +0530

    sol1 for chunked

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,16 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunked = []
+    for(var elem of array){
+        let last = chunked[chunked.length - 1]
+        if(!last  || last.length ==  size )
+            chunked.push([elem])
+        else    
+            chunked[chunked.length - 1].push(elem)    
+    }
+    return chunked
+}
 
 module.exports = chunk;
