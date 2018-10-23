39875d41875c10a36b2be8f69e5fdaf8369c822f
Fahid M <fahidroid@gmail.com>
2018-10-23 12:48:05 +0530

    sol1 for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var charMap = {};
+    var max = -Infinity
+    for(var ch of str){
+        if(charMap[ch]) 
+            charMap[ch]++
+        else 
+            charMap[ch] = 1   
+    }
+
+    for(var ch in charMap){
+        if(charMap[ch] > max)
+            max = ch;
+    }
+    return max;
+}
 
 module.exports = maxChar;
